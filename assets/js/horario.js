3bd072a607aa537c50691a22d38fdc3794c21c44
fescarvalho <fernando_scarvalho@hotmail.com>
2022-12-20 22:09:14 -0300

    add function time open/close

---
+++ assets/js/horario.js
@@ -0,0 +1,15 @@
+const bullet = document.querySelector(".funcionamento");
+const now = new Date();
+const dia = now.getDay();
+const hours = now.getHours();
+
+verficarHora();
+const time = setInterval(verficarHora, 60000);
+
+function verficarHora() {
+  if (hours >= 22 || hours < 07 || dia === 7) {
+    bullet.classList.add("fechado");
+  } else {
+    bullet.classList.add("aberto");
+  }
+}
